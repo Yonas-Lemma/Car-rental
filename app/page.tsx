297806c8d8b5ee5db490077dbc7b4d32d6b3b776
Yonas Lemma <143127905+Yonas-Lemma@users.noreply.github.com>
2025-04-25 12:45:46 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -187,8 +187,8 @@ export default function Home() {
       contact: {
         title: "Kontakt",
         address: "Lindleya 16, 02-013 m 301 Warszawa",
-        phone: "+48666644883",
-        email: "info@eurotaxi.pl",
+        phone: "+48 66 664 4883",
+        email: "Kilanibmci@gmail.com",
         followUs: "Obserwuj nas",
         copyright: "© 2023 Euro Taxi. Wszelkie prawa zastrzeżone.",
         quickLinks: "Szybkie linki",
